8c69d79870efd53ccc7d1ecacd821c65d5f11fe4
bartosz <bziembanski@gmail.com>
2021-03-09 23:00:58 +0100

    innit

---
+++ server/src/database.js
@@ -0,0 +1,28 @@
+const Pool = require('pg').Pool;
+const db_conf = require('../database-confidential');
+
+const pool = new Pool({
+    user: db_conf.user,
+    host: db_conf.host,
+    database: db_conf.database,
+    password: db_conf.password,
+    port: db_conf.port,
+    ssl:{
+        rejectUnauthorized: false
+    }
+});
+
+const getUsers = (req, res) => {
+    pool.query('SELECT * FROM users ORDER BY user_id ASC', 
+        (error, results) => {
+            if(error){
+                throw error;
+            }
+            res.status(200).json(results.rows);
+        }
+    );
+}
+
+module.exports = {
+    getUsers
+}
