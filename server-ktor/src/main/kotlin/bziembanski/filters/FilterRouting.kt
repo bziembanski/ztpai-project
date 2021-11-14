d9eb7d4907a1f25a5d5fd30f188ffb30555cec45
bartosz <bziembanski@gmail.com>
2021-11-14 17:37:43 +0100

    using inline kotlinx.serialization

--- server-ktor/src/main/kotlin/bziembanski/filters/FilterRouting.kt
+++ server-ktor/src/main/kotlin/bziembanski/filters/FilterRouting.kt
@@ -4,46 +4,41 @@ import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 fun Route.filters() {
-    val data: List<Filter> = listOf(
-        SelectFilter(
-            name = "Sortowanie",
-            data = listOf(
-                "Odległość rosnącą",
-                "Odległość malejąco",
-                "Wynagrodzenie rosnąco",
-                "Wynagrodzenie malejąco"
-            )
-        ),
-        SliderFilter(
-            name = "Wynagrodzenie za godzinę",
-            data = SliderDataType(
-                min = 0,
-                max = 150,
-                step = 5,
-                value = Pair(0, 100)
-            )
-        ),
-        CheckboxFilter(
-            name = "Kategoria",
-            data = listOf(
-                "mechanika",
-                "ogrodnictwo",
-                "elektryka",
-                "budownictwo",
-            )
-        ),
-    )
-    val dataString = Json.encodeToString(data)
     route("/filters") {
         get {
-            call.respondText(
-                text = dataString,
-                status = HttpStatusCode.OK,
-                contentType = ContentType.Application.Json
+            call.respond(
+                HttpStatusCode.OK,
+                listOf(
+                    SelectFilter(
+                        name = "Sortowanie",
+                        data = listOf(
+                            "Odległość rosnącą",
+                            "Odległość malejąco",
+                            "Wynagrodzenie rosnąco",
+                            "Wynagrodzenie malejąco"
+                        )
+                    ),
+                    SliderFilter(
+                        name = "Wynagrodzenie za godzinę",
+                        data = SliderDataType(
+                            min = 0,
+                            max = 150,
+                            step = 5,
+                            value = Pair(0, 100)
+                        )
+                    ),
+                    CheckboxFilter(
+                        name = "Kategoria",
+                        data = listOf(
+                            "mechanika",
+                            "ogrodnictwo",
+                            "elektryka",
+                            "budownictwo",
+                        )
+                    )
+                )
             )
         }
     }
--- server-ktor/src/main/kotlin/bziembanski/plugins/Serialization.kt
+++ server-ktor/src/main/kotlin/bziembanski/plugins/Serialization.kt
@@ -3,9 +3,14 @@ package bziembanski.plugins
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.serialization.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            useArrayPolymorphism = false
+        })
     }
 }
